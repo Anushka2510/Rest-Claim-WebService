13d3d2e72971e26949c724b5cd35f2b6bf4b3577
Anushka2510 <anushkajaiswal0110@gmail.com>
2023-06-09 11:18:11 +0530

    Add service test

---
+++ src/test/groovy/com/allstate/restclaimwebservice/service/PolicyDetailsServiceSpec.groovy
@@ -0,0 +1,44 @@
+package com.allstate.restclaimwebservice.service
+
+import com.allstate.restclaimwebservice.model.PolicyDetails
+import com.allstate.restclaimwebservice.model.PolicyDetailsRequest
+import com.allstate.restclaimwebservice.model.PolicyDetailsResponse
+import org.springframework.ws.client.core.WebServiceTemplate
+import spock.lang.Specification
+
+class PolicyDetailsServiceSpec extends Specification{
+
+    def webServiceTemplate = Mock(WebServiceTemplate)
+
+
+    def "should return policy details response"() {
+        given:
+
+        def policyDetailsService = new PolicyDetailsService(webServiceTemplate)
+        def request = new PolicyDetailsRequest()
+        def expectedResponse = new PolicyDetailsResponse()
+
+        when:
+        def result = policyDetailsService.getPolicyDetails(request)
+
+        then:
+        1 * webServiceTemplate.marshalSendAndReceive(request) >> expectedResponse
+        result == expectedResponse
+    }
+
+    def "should throw exception when claim number does not exist"() {
+        given:
+        PolicyDetailsService service = new PolicyDetailsService(webServiceTemplate)
+        PolicyDetailsRequest request = new PolicyDetailsRequest()
+
+        webServiceTemplate.marshalSendAndReceive(request) >> { throw new Exception("Claim Number Does not exist exception") }
+
+        when:
+        service.getPolicyDetails(request)
+
+        then:
+        Exception e = thrown()
+        e.message == "Claim Number Does not exist exception"
+    }
+
+}
